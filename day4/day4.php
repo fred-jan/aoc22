9429f08672775eb17dcf9447d0890cfc6b681ceb
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2022-12-04 08:51:03 +0100

    Solution to day 4 part 2 in PHP

--- day4/day4.php
+++ day4/day4.php
@@ -9,3 +9,12 @@ $fully_overlapping_ranges = \count(\array_filter(\array_map(
 )));
 
 \printf("There are %d elf pairs with fully overlapping ranges (part 1)\n", $fully_overlapping_ranges);
+
+$partially_overlapping_ranges = \count(\array_filter(\array_map(
+    static fn (string $line) =>
+        \preg_match('/(\d+)-(\d+),(\d+)-(\d+)/', $line, $matches)
+        && \count(\array_intersect(\range($matches[1], $matches[2]), range($matches[3], $matches[4]))) > 0,
+    \array_filter(\explode("\n", \file_get_contents(dirname(__FILE__) . '/input.txt')))
+)));
+
+\printf("There are %d elf pairs with overlapping ranges (part 2)\n", $partially_overlapping_ranges);
